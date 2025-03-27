2c0489f22784018f3f5de0a9fb2de0a519be3ea1
AnushriJawahar96 <s223514497@deakin.edu.au>
2025-03-27 21:16:33 +1100

    Full Code for Week 3 Task

---
+++ Week3/public/js/script.js
@@ -0,0 +1,26 @@
+const clickMe = () => {
+    alert("Thanks for clicking me. Hope you have a nice day!");
+    $('#modal1').modal('open'); // Open the modal after the alert
+    }
+    const submitForm = () => {
+        let formData = {
+            first_name: $('#first_name').val(),
+            last_name: $('#last_name').val(),
+            password: $('#password').val(),
+            email: $('#email').val()
+        };
+        console.log("Form Data Submitted: ", formData);
+    };
+    $(document).ready(function(){
+        $('.materialboxed').materialbox();
+        $('.modal').modal(); // Ensure modal is initialized
+    
+        $('#clickMeButton').click(() => {
+            clickMe(); // Show alert and open modal
+        });
+    
+        $('#formSubmit').click(() => {
+            submitForm();
+        });
+    });
+    
